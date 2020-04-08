e951236c6a1840979b33837a56f3ca4f5efcb61f
jagmeet singh <jagmeet.alagh@gmail.com>
2020-04-08 23:31:28 +0530

    Updates

---
+++ precache-manifest.842bdcc88cc20cc45a71989cd08f5df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d7dd1a142a771d5afd349f655b8137",
+    "url": "/Rest-countries-react/index.html"
+  },
+  {
+    "revision": "c301d80239e5806e78a9",
+    "url": "/Rest-countries-react/static/css/main.f976cb34.chunk.css"
+  },
+  {
+    "revision": "63062dabcef700b2a7ff",
+    "url": "/Rest-countries-react/static/js/2.6357d7bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rest-countries-react/static/js/2.6357d7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c301d80239e5806e78a9",
+    "url": "/Rest-countries-react/static/js/main.2199de15.chunk.js"
+  },
+  {
+    "revision": "5e3cf177f1f6c87533cf",
+    "url": "/Rest-countries-react/static/js/runtime-main.1da70441.js"
+  }
+]);
